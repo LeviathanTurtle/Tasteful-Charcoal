bbddb7acbb74c4f12018983313facc1f2ab0e685
LeviathanTurtle <wbwads@gmail.com>
2025-05-15 20:51:03 -0400

    user config file

---
+++ config.py
@@ -0,0 +1,13 @@
+
+# 
+# William Wadsworth
+# 
+
+# collection of mods to check
+# "mod_name" : mod_id,
+MODS_IN_QUESTION: dict[str,int] = {
+    "create" : 328085,
+}
+
+# set to true if you want to include non-release versions
+include_prereleases: bool = False
